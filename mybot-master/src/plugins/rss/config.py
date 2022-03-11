49b20a8fd3e59989f521eb13cac66bc45cc6afc2
NoNormalCreeper <2560359315@qq.com>
2022-03-11 08:40:04 +0800

    delete useless files

--- mybot-master/src/plugins/rss/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Config(BaseSettings):
-    rss_update_cron: list = ['0', '*/5', '*', '*', '*', '*']
-    rss_info_update_cron: list = ['0', '0', '5', '*', '*', '*']
-
-    class Config:
-        extra = "ignore"
