c00ab2e1f0ce9cf666270599f443f2b14bac6067
NoNormalCreeper <2560359315@qq.com>
2022-03-13 22:59:23 +0800

    rename

---
+++ Kori-bot/src/plugins/test/test_saying.py
@@ -0,0 +1,6 @@
+from nonebot.adapters import Bot, Event
+from nonebot.adapters.onebot.v11 import MessageSegment, Message
+from nonebot.typing import T_State
+from nonebot.params import State, CommandArg
+from nonebot.matcher import Matcher
+from nonebot import on_command, get_bot
