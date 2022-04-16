2f3fe4201205a7cf1037127aa1c28efe5e434fa5
NoNormalCreeper <2560359315@qq.com>
2022-04-16 20:10:48 +0800

    🔥 delete unecessary codes

--- Kori-bot/src/plugins/wolfram/get_answer.py
+++ Kori-bot/src/plugins/wolfram/get_answer.py
@@ -1,6 +1,4 @@
 import requests as re
-import shutil
-import json
 import urllib
 
 
@@ -14,26 +12,10 @@ def get_calc(question: str):
     resp=re.get(url, stream=True)
     if 'Wolfram|Alpha did not understand your input' in resp.text:
         raise Exception(resp.text)
-    # with open("answer.gif","wb") as pic:
-    #     # try:
-    #         # pic.write(url.content)
-    #     # for chunk in resp.iter_content(1024):
-    #     #     pic.write(chunk)
-    #     resp.raw.decode_content = True
-    #     shutil.copyfileobj(resp.raw, pic)
-
-    #     # except:
-    #     #     return str(resp)
     return resp.content
 
-    # return 0
 
 def get_tellme(question: str):
     url=(url_tellme.format(urllib.parse.quote(question), API_key))
     resp=re.get(url)
-    # try:
-    #     result=resp.text
-    # except:
-    #     return str(resp)
     return resp.text
-    # return resp.content.decode(resp.apparent_encoding).encode('ascii')
