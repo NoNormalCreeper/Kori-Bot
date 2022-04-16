2805395603d2d7da437e58de541b12cff905c4e4
NoNormalCreeper <2560359315@qq.com>
2022-04-16 20:04:31 +0800

    🎨 optimize arguments get method

--- Kori-bot/src/plugins/wolfram/__init__.py
+++ Kori-bot/src/plugins/wolfram/__init__.py
@@ -1,72 +1,21 @@
 from nonebot.adapters import Bot, Event, Message
 from nonebot.adapters.onebot.v11 import MessageSegment
 from nonebot.typing import T_State
-from nonebot.params import State
+from nonebot.params import State, CommandArg
 from nonebot.matcher import Matcher
 from nonebot import on_command
 # import requests
 import json
 # import get_answer as ga
-# from get_answer import *
-
-
-# ----- get_answer starts -----
-import requests as re
-import shutil
-import json
-import urllib
-
-
-url_calc="http://api.wolframalpha.com/v1/simple?appid={1}&i={0}&units=metric"
-url_tellme="https://api.wolframalpha.com/v1/result?appid={1}&i={0}&units=metric"
-API_key="X9H8TV-QKGR82YQUY"
-
-
-def get_calc(question: str):
-    url=(url_calc.format(urllib.parse.quote(question), API_key))
-    resp=re.get(url, stream=True)
-    # with open("answer.gif","wb") as pic:
-    #     # try:
-    #         # pic.write(url.content)
-    #     # for chunk in resp.iter_content(1024):
-    #     #     pic.write(chunk)
-    #     resp.raw.decode_content = True
-    #     shutil.copyfileobj(resp.raw, pic)
-
-    #     # except:
-    #     #     return str(resp)
-    return resp.content
-
-    # return 0
-
-def get_tellme(question: str):
-    url=(url_tellme.format(urllib.parse.quote(question), API_key))
-    resp=re.get(url)
-    # try:
-    #     result=resp.text
-    # except:
-    #     return str(resp)
-    return resp.text
-    # return resp.content.decode(resp.apparent_encoding).encode('ascii')
-
-# ----- get_answer ends -----
-
-
-def removeprefix(string, prefix):
-    if not (isinstance(string, str) and isinstance(prefix, str)):
-        raise TypeError('Param value type error')
-    if string.startswith(prefix):
-        return string[len(prefix):]
-    return string
-
+from .get_answer import *
 
 
 tellme = on_command("tellme")
 
 @tellme.handle()
-async def tellme_handle(bot: Bot, event: Event, matcher: Matcher, state: T_State = State()):
-    arg = removeprefix((str(event.get_message()).strip())[1:], "tellme")
-    print(("args:",arg))
+async def tellme_handle(bot: Bot, event: Event, matcher: Matcher, state: T_State = State(), arg: Message = CommandArg()):
+    arg = arg.extract_plain_text().strip()
+    # print(("args:",arg))
     result = get_tellme(arg)
 
     await tellme.send(('[Result]\n'+result))
@@ -74,10 +23,9 @@ async def tellme_handle(bot: Bot, event: Event, matcher: Matcher, state: T_State
 
 calc = on_command("calc", aliases={'计算'})
 
-
 @calc.handle()
-async def calc_handle(bot: Bot, event: Event, matcher: Matcher, state: T_State = State()):
-    arg = removeprefix((str(event.get_message()).strip())[1:], "calc")
+async def calc_handle(bot: Bot, event: Event, matcher: Matcher, state: T_State = State(), arg: Message = CommandArg()):
+    arg = arg.extract_plain_text().strip()
     try:
         result = get_calc(arg)
         await calc.send(MessageSegment.image(result))
--- Kori-bot/src/plugins/wolfram/get_answer.py
+++ Kori-bot/src/plugins/wolfram/get_answer.py
@@ -9,7 +9,7 @@ url_tellme="https://api.wolframalpha.com/v1/result?appid={1}&i={0}&units=metric"
 API_key="X9H8TV-QKGR82YQUY"
 
 
-def calc(question: str):
+def get_calc(question: str):
     url=(url_calc.format(urllib.parse.quote(question), API_key))
     resp=re.get(url, stream=True)
     if 'Wolfram|Alpha did not understand your input' in resp.text:
@@ -28,7 +28,7 @@ def calc(question: str):
 
     # return 0
 
-def tellme(question: str):
+def get_tellme(question: str):
     url=(url_tellme.format(urllib.parse.quote(question), API_key))
     resp=re.get(url)
     # try:
