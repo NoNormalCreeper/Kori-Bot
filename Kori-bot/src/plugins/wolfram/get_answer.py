c894ccc972c0be341e98c8af76ca6584d150cf1f
NoNormalCreeper <2560359315@qq.com>
2022-04-22 18:17:07 +0800

    🚑 hotfix

--- Kori-bot/src/plugins/wolfram/get_answer.py
+++ Kori-bot/src/plugins/wolfram/get_answer.py
@@ -1,12 +1,13 @@
+import imp
 import requests as re
 import urllib
-import nonebot
+from nonebot import get_driver
 
 
 url_calc = "http://api.wolframalpha.com/v1/simple?appid={1}&i={0}&units=metric"
 url_tellme = "https://api.wolframalpha.com/v1/result?appid={1}&i={0}&units=metric"
 try:
-    API_key = nonebot.config.WOLFRAM_API_KEY
+    API_key = get_driver().config.WOLFRAM_API_KEY
 except Exception as e:
     raise Exception("请先在配置文件中配置 WOLFRAM_API_KEY 哦~\n{str(e)}")
 
