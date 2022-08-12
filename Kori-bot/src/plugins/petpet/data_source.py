66474a3cfe90ebb7022545b6f1ec31b650dfacb6
NoNormalCreeper <2560359315@qq.com>
2022-08-12 10:50:51 +0800

    ✨ Update petpet plugin

--- Kori-bot/src/plugins/petpet/data_source.py
+++ Kori-bot/src/plugins/petpet/data_source.py
@@ -1,76 +1,75 @@
-from io import BytesIO
-from typing import List, Union
-
-from .download import download_url, download_avatar
-from .utils import to_image
-from .models import UserInfo, Command
+from .utils import Meme
 from .functions import *
 
-commands = [
-    Command(("摸", "摸摸", "摸头", "摸摸头", "rua"), petpet, arg_num=1),
-    Command(("亲", "亲亲"), kiss),
-    Command(("贴", "贴贴", "蹭", "蹭蹭"), rub),
-    Command(("顶", "玩"), play),
-    Command(("拍",), pat),
-    Command(("撕",), rip, arg_num=2),
-    Command(("丢", "扔"), throw),
-    Command(("抛", "掷"), throw_gif),
-    Command(("爬",), crawl, arg_num=1),
-    Command(("精神支柱",), support),
-    Command(("一直",), always, convert=False),
-    Command(("加载中",), loading, convert=False),
-    Command(("转",), turn),
-    Command(("小天使",), littleangel, convert=False, arg_num=1),
-    Command(("不要靠近",), dont_touch),
-    Command(("一样",), alike),
-    Command(("滚",), roll),
-    Command(("玩游戏", "来玩游戏"), play_game, convert=False, arg_num=1),
-    Command(("膜", "膜拜"), worship),
-    Command(("吃",), eat),
-    Command(("啃",), bite),
-    Command(("出警",), police),
-    Command(("警察",), police1, convert=False),
-    Command(("问问", "去问问"), ask, convert=False, arg_num=1),
-    Command(("舔", "舔屏", "prpr"), prpr, convert=False),
-    Command(("搓",), twist),
-    Command(("墙纸",), wallpaper, convert=False),
-    Command(("国旗",), china_flag),
-    Command(("交个朋友",), make_friend, convert=False, arg_num=1),
-    Command(("继续干活",), back_to_work, convert=False),
-    Command(("完美", "完美的"), perfect, convert=False),
-    Command(("关注",), follow, arg_num=1),
-    Command(("我朋友说", "我有个朋友说"), my_friend, arg_num=10),
-    Command(("这像画吗",), paint, convert=False),
-    Command(("震惊",), shock),
-    Command(("兑换券",), coupon, arg_num=2),
-    Command(("听音乐",), listen_music),
-    Command(("典中典",), dianzhongdian, convert=False, arg_num=3),
-    Command(("哈哈镜",), funny_mirror),
-    Command(("永远爱你",), love_you),
-    Command(("对称",), symmetric, convert=False, arg_num=1),
-    Command(("安全感",), safe_sense, convert=False, arg_num=2),
-    Command(("永远喜欢", "我永远喜欢"), always_like, convert=False, arg_num=10),
-    Command(("采访",), interview, arg_num=1),
-    Command(("打拳",), punch, convert=False),
-    Command(("群青",), cyan),
+memes = [
+    Meme("universal", universal, ("万能表情", "空白表情")),
+    Meme("petpet", petpet, ("摸", "摸摸", "摸头", "rua"), r"摸摸?头?|rua"),
+    Meme("kiss", kiss, ("亲", "亲亲"), r"亲亲?"),
+    Meme("rub", rub, ("贴", "贴贴", "蹭", "蹭蹭"), r"贴贴?|蹭蹭?"),
+    Meme("play", play, ("顶", "玩")),
+    Meme("pat", pat, ("拍",)),
+    Meme("rip", rip, ("撕",)),
+    Meme("throw", throw, ("丢", "扔")),
+    Meme("throw_gif", throw_gif, ("抛", "掷")),
+    Meme("crawl", crawl, ("爬",)),
+    Meme("support", support, ("精神支柱",)),
+    Meme("always", always, ("一直",)),
+    Meme("loading", loading, ("加载中",)),
+    Meme("turn", turn, ("转",)),
+    Meme("littleangel", littleangel, ("小天使",)),
+    Meme("dont_touch", dont_touch, ("不要靠近",)),
+    Meme("alike", alike, ("一样",)),
+    Meme("roll", roll, ("滚",)),
+    Meme("play_game", play_game, ("玩游戏",), r"来?玩游戏"),
+    Meme("worship", worship, ("膜", "膜拜"), r"膜拜?"),
+    Meme("eat", eat, ("吃",)),
+    Meme("bite", bite, ("啃",)),
+    Meme("police", police, ("出警",)),
+    Meme("police1", police1, ("警察",)),
+    Meme("ask", ask, ("问问",), r"去?问问"),
+    Meme("prpr", prpr, ("舔", "舔屏", "prpr"), r"舔屏?|prpr"),
+    Meme("twist", twist, ("搓",)),
+    Meme("wallpaper", wallpaper, ("墙纸",)),
+    Meme("china_flag", china_flag, ("国旗",)),
+    Meme("make_friend", make_friend, ("交个朋友",)),
+    Meme("back_to_work", back_to_work, ("继续干活", "打工人")),
+    Meme("perfect", perfect, ("完美",), r"完美的?"),
+    Meme("follow", follow, ("关注",)),
+    Meme("my_friend", my_friend, ("我朋友{name}说",), r"我(?:有个)?朋友(?P<name>.*?)说"),
+    Meme("paint", paint, ("这像画吗",)),
+    Meme("shock", shock, ("震惊",)),
+    Meme("coupon", coupon, ("兑换券",)),
+    Meme("listen_music", listen_music, ("听音乐",)),
+    Meme("dianzhongdian", dianzhongdian, ("典中典", "黑白草图")),
+    Meme("funny_mirror", funny_mirror, ("哈哈镜",)),
+    Meme("love_you", love_you, ("永远爱你",)),
+    Meme("symmetric", symmetric, ("对称",)),
+    Meme("safe_sense", safe_sense, ("安全感",)),
+    Meme("always_like", always_like, ("我永远喜欢",), r"我?永远喜欢"),
+    Meme("interview", interview, ("采访",)),
+    Meme("punch", punch, ("打拳",)),
+    Meme("cyan", cyan, ("群青",)),
+    Meme("pound", pound, ("捣",)),
+    Meme("thump", thump, ("捶",)),
+    Meme("need", need, ("需要", "你可能需要")),
+    Meme("cover_face", cover_face, ("捂脸",)),
+    Meme("knock", knock, ("敲",)),
+    Meme("garbage", garbage, ("垃圾", "垃圾桶"), r"垃圾桶?"),
+    Meme("whyatme", whyatme, ("为什么@我",), r"为什么(?:@|at)我"),
+    Meme("decent_kiss", decent_kiss, ("像样的亲亲",)),
+    Meme("jiujiu", jiujiu, ("啾啾",)),
+    Meme("suck", suck, ("吸", "嗦")),
+    Meme("hammer", hammer, ("锤",)),
+    Meme("tightly", tightly, ("紧贴", "紧紧贴着")),
+    Meme("distracted", distracted, ("注意力涣散",)),
+    Meme("anyasuki", anyasuki, ("阿尼亚喜欢",)),
+    Meme("thinkwhat", thinkwhat, ("想什么",)),
+    Meme("keepaway", keepaway, ("远离",)),
+    Meme("marriage", marriage, ("结婚申请", "结婚登记")),
+    Meme("painter", painter, ("小画家",)),
+    Meme("repeat", repeat, ("复读",)),
+    Meme("anti_kidnap", anti_kidnap, ("防诱拐",)),
+    Meme("charpic", charpic, ("字符画",)),
+    Meme("mywife", mywife, ("这是我的老婆",), r"这是(?P<ta>\S+)的(?P<name>\S+)"),
+    Meme("walnutpad", walnutpad, ("胡桃平板",)),
 ]
-
-
-async def download_image(user: UserInfo, convert: bool = True):
-    img = None
-    if user.qq:
-        img = await download_avatar(user.qq)
-    elif user.img_url:
-        img = await download_url(user.img_url)
-
-    if img:
-        user.img = to_image(img, convert)
-
-
-async def make_image(
-    command: Command, sender: UserInfo, users: List[UserInfo], args: List[str] = []
-) -> Union[str, BytesIO]:
-    await download_image(sender, command.convert)
-    for user in users:
-        await download_image(user, command.convert)
-    return await command.func(users, sender=sender, args=args)
